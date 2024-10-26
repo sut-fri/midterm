c555f57716e1eeead94c074ccfb4f90eeafbe435
sut-fri <srf8625@nyu.edu>
2024-10-26 12:19:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const key = document.querySelector(".ull-container a");
 const effect = document.querySelector("body");
-const lamb = document.querySelector('#lamb');
-const br = document.querySelector('.br');
+const lamb = document.querySelector("#lamb");
+const br2 = document.querySelector(".br2");
+const br3 = document.querySelector(".br3");
+const br4 = document.querySelector(".br4");
 
 key.addEventListener("mouseenter", () => {
     effect.style.background = "#A02334";
     lamb.style.color = "#FCFAEE";
-    br.style.color = "#FCFAEE";
+    if (br2) br2.style.color = "#FCFAEE";
+    if (br3) br3.style.color = "#FCFAEE";
+    if (br4) br4.style.color = "#FCFAEE";
 });
 
 key.addEventListener("mouseleave", () => {
     effect.style.background = "";
     lamb.style.color = "";
-    br.style.color = "";
+    if (br2) br2.style.color = "";
+    if (br3) br3.style.color = "";
+    if (br4) br4.style.color = "";
 });
